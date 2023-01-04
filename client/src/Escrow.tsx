4ea7754db4146eb06104b28ce22ab79a6132d07e
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-01-05 00:20:18 +0400

    replicate sample project but in TS

---
+++ client/src/Escrow.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Escrow = ({ address, arbiter, beneficiary, value, handleApprove }) => {
+  return (
+    <div className="existing-contract">
+      <ul className="fields">
+        <li>
+          <div> Arbiter </div>
+          <div> {arbiter} </div>
+        </li>
+        <li>
+          <div> Beneficiary </div>
+          <div> {beneficiary} </div>
+        </li>
+        <li>
+          <div> Value </div>
+          <div> {value} </div>
+        </li>
+        <div
+          className="button"
+          id={address}
+          onClick={(e) => {
+            e.preventDefault();
+
+            handleApprove();
+          }}
+        >
+          Approve
+        </div>
+      </ul>
+    </div>
+  );
+};
+
+export default Escrow;
